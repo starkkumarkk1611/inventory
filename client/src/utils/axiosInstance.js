bac64321b85831b3e4f8f844ac4e64bc9f0684ee
Kundan Kumar Kushwaha <starkkumarkk1611@gmail.com>
2023-09-14 21:48:28 +0530

    base url

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const instance = axios.create( {
-  baseURL: "http://localhost:3500/api/"
+const instance = axios.create({
+  baseURL: "https://nvcticlubinventory.onrender.com/api/"
 })
 
 export default instance;
