25b1e6ccf162de375e5474f63ec64758c05b9814
Kundan Kumar Kushwaha <starkkumarkk1611@gmail.com>
2023-09-14 21:49:51 +0530

    baseURL

--- utilities/axiosInstance.js
+++ utilities/axiosInstance.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
-const os = require( "os" );
+const os = require("os");
 
 const port = process.env.PORT || 3500;
 const host = process.env.HOST || os.hostname();
 
 const instance = axios.create({
-  baseURL: `http://${host}:${port}/api/`
+  baseURL: `https://nvcticlubinventory.onrender.com/api/`
 });
 
 module.exports = instance;
